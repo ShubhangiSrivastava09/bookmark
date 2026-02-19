ad4fffe839593afbef29e99bf205a8006b47e145
ShubhangiSrivastava09 <shubhangi.sri09@gmail.com>
2026-02-19 20:46:46 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${location.origin}/auth/callback`,
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
     });
   };
